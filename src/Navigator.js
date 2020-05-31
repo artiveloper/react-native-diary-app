4b03e55ad512194219d396684bdf75a871698ef0
RAVIKIM <artiveloper@gmail.com>
2020-05-31 10:29:10 +0900

    feat: 글쓰기 페이지를 추가한다

--- src/Navigator.js
+++ src/Navigator.js
@@ -1,50 +1,89 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Ionicons } from '@expo/vector-icons';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createStackNavigator} from '@react-navigation/stack';
+import {Ionicons} from '@expo/vector-icons';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {TouchableOpacity} from 'react-native';
 
 import ListScreen from 'screens/ListScreen';
 import BookmarkScreen from 'screens/BookmarkScreen';
+import EditScreen from 'screens/EditScreen';
+import BlankScreen from 'screens/BlankScreen';
+import {useNavigation} from '@react-navigation/core';
 
 const Tab = createBottomTabNavigator();
+const AppStack = createStackNavigator();
 
 const TabNavigator = () => {
+    const navigation = useNavigation();
     return (
         <SafeAreaProvider>
-            <NavigationContainer>
-                <Tab.Navigator
-                    screenOptions={({ route }) => ({
-                        tabBarIcon: ({ focused, color }) => {
-                            let iconName;
-
-                            if (route.name === 'List') {
-                                iconName = focused
-                                    ? 'md-list-box'
-                                    : 'md-list-box';
-                            } else if (route.name === 'Bookmark') {
-                                iconName = focused
-                                    ? 'md-heart'
-                                    : 'md-heart';
-                            }
-
-                            return <Ionicons name={iconName} size={25} color={color} />;
-                        },
-                    })}
-                    tabBarOptions={{
-                        activeTintColor: '#424242',
-                        inactiveTintColor: '#9E9E9E',
-                        showLabel: false,
-                        borderTopColor: '#BDBDBD',
-                        backgroundColor: '#FFFFFF',
+            <Tab.Navigator
+                tabBarOptions={{
+                    activeTintColor: '#424242',
+                    inactiveTintColor: '#9E9E9E',
+                    showLabel: false,
+                    borderTopColor: '#BDBDBD',
+                    backgroundColor: '#FFFFFF',
+                }}
+            >
+                <Tab.Screen
+                    name="List"
+                    component={ListScreen}
+                    options={{
+                        tabBarIcon: ({color}) => {
+                            return <Ionicons name="md-list-box" size={25} color={color} />
+                        }
                     }}
-                >
-                    <Tab.Screen name="List" component={ListScreen} />
-                    <Tab.Screen name="Bookmark" component={BookmarkScreen} />
-                </Tab.Navigator>
-            </NavigationContainer>
+                />
+                <Tab.Screen
+                    name="AddButton"
+                    component={BlankScreen}
+                    options={{
+                        tabBarIcon: () => {
+                            return <Ionicons name="ios-add-circle" size={36} color="#DA5746" />
+                        },
+                        tabBarButton: () => (
+                            <TouchableOpacity
+                                opacity={0.8}
+                                onPress={() => navigation.navigate('Edit')}
+                                style={{
+                                    justifyContent: 'center'
+                                }}
+                            >
+                                <Ionicons name="ios-add-circle" size={36} color="#DA5746" />
+                            </TouchableOpacity>
+                        )
+                    }}
+                />
+                <Tab.Screen
+                    name="Bookmark"
+                    component={BookmarkScreen}
+                    options={{
+                        tabBarIcon: ({color}) => {
+                            return <Ionicons name="md-heart" size={25} color={color} />
+                        }
+                    }}
+                />
+            </Tab.Navigator>
         </SafeAreaProvider>
     )
 }
 
-export default TabNavigator;
+const AppNavigator = () => {
+    return (
+        <NavigationContainer>
+            <AppStack.Navigator
+                initialRouteName="Tab"
+                mode="modal"
+                headerMode="none"
+            >
+                <AppStack.Screen name="Tab" component={TabNavigator} />
+                <AppStack.Screen name="Edit" component={EditScreen} />
+            </AppStack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+export default AppNavigator;
---
+++ src/screens/BlankScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const BlankScreen = () => {
+    return (
+        <React.Fragment>
+        </React.Fragment>
+    );
+}
+
+export default BlankScreen;
