32c12cd267fb377752731748b412926ac8470b07
RAVIKIM <artiveloper@gmail.com>
2020-06-01 20:06:31 +0900

    fix: 즐겨찾기 화면에 필요한 목업 데이터를 추가한다

--- src/screens/BookmarkScreen.js
+++ src/screens/BookmarkScreen.js
@@ -5,11 +5,28 @@ import {
 } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Header from 'components/Header';
+import ArticleItem from 'components/ArticleItem';
 
 const BookmarkScreen = () => {
     return (
         <SafeAreaView style={styles.container}>
             <Header title="즐겨찾기" />
+            <ArticleItem
+                article={{
+                    id: 1,
+                    title: '청춘의 문장들',
+                    content: '사이에 있는 것들, 쉽게 바뀌는 것들, 덧없이 사라지는 것들이 여전히 내 마음을 잡아끈다.',
+                    date: '2019년 4월 7일',
+                }}
+            />
+            <ArticleItem
+                article={{
+                    id: 1,
+                    title: '청춘의 문장들',
+                    content: '사이에 있는 것들, 쉽게 바뀌는 것들, 덧없이 사라지는 것들이 여전히 내 마음을 잡아끈다.',
+                    date: '2019년 4월 7일',
+                }}
+            />
         </SafeAreaView>
     );
 };
@@ -17,6 +34,7 @@ const BookmarkScreen = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        backgroundColor: '#FFF',
     }
 })
 
