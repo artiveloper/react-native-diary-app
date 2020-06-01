7c5227bac10bcddaea31f1232f243546ad2679f5
RAVIKIM <artiveloper@gmail.com>
2020-06-01 20:04:41 +0900

    feat: 길게 눌러 수정하는 화면으로 이동하도록 한다

--- src/screens/ViewScreen.js
+++ src/screens/ViewScreen.js
@@ -2,19 +2,33 @@ import React from 'react';
 import {
     Text,
     ScrollView,
+    TouchableOpacity,
     StyleSheet,
 } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import ViewHeader from 'components/ViewHeader';
+import {useNavigation} from '@react-navigation/core';
 
 const ViewScreen = () => {
+
+    const navigation = useNavigation();
+
+    const handleLongPress = () => {
+        navigation.navigate('Edit')
+    }
+
     return (
         <SafeAreaView style={styles.container}>
             <ViewHeader />
             <ScrollView>
-                <Text style={styles.content}>
-                    블라블라.....
-                </Text>
+                <TouchableOpacity
+                    activeOpacity={0.8}
+                    onLongPress={handleLongPress}
+                >
+                    <Text style={styles.content}>
+                        블라블라.....
+                    </Text>
+                </TouchableOpacity>
                 <Text style={styles.date}>
                     2019년 9월 14일
                 </Text>
