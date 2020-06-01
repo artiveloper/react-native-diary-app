ad1a25e28421510073a65bd762370320d74d4650
RAVIKIM <artiveloper@gmail.com>
2020-06-01 19:43:10 +0900

    feat: 상태바를 설정을 추가한다

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import {StatusBar} from 'react-native';
 import TabNavigator from 'Navigator';
 
 export default function App() {
     return (
-        <TabNavigator/>
+        <React.Fragment>
+            <StatusBar barStyle="dark-content"/>
+            <TabNavigator/>
+        </React.Fragment>
     );
 }
