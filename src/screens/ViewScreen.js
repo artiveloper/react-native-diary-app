800a91266e0265cac83bc215720144b88324a834
RAVIKIM <artiveloper@gmail.com>
2020-06-01 20:00:03 +0900

    fix: 글 읽기 화면 마크업 수정

--- src/screens/ViewScreen.js
+++ src/screens/ViewScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {
     Text,
+    ScrollView,
     StyleSheet,
 } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -10,6 +11,14 @@ const ViewScreen = () => {
     return (
         <SafeAreaView style={styles.container}>
             <ViewHeader />
+            <ScrollView>
+                <Text style={styles.content}>
+                    블라블라.....
+                </Text>
+                <Text style={styles.date}>
+                    2019년 9월 14일
+                </Text>
+            </ScrollView>
         </SafeAreaView>
     )
 }
@@ -18,7 +27,18 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
-
+    content: {
+        padding: 20,
+        fontSize: 16,
+        lineHeight: 20,
+        color: '#424242',
+    },
+    date: {
+        padding: 20,
+        paddingTop: 20,
+        fontSize: 12,
+        color: '#BDBDBD',
+    }
 })
 
 export default ViewScreen;
