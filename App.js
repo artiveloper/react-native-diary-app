124ccb8f85de1a15358492d07d330f072a18b20d
RAVIKIM <artiveloper@gmail.com>
2020-06-09 20:38:59 +0900

    fix: 컴포넌트 이름을 변경한다

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import {StatusBar} from 'react-native';
-import TabNavigator from 'Navigator';
+import AppNavigator from 'Navigator';
 import 'mobx-react-lite/batchingForReactNative'
 
 export default function App() {
     return (
         <React.Fragment>
             <StatusBar barStyle="dark-content"/>
-            <TabNavigator/>
+            <AppNavigator/>
         </React.Fragment>
     );
 }
